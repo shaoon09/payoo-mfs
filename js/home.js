581be02926651ab4433796e829122a61319b3593
shaoon09 <iiucclass1@gmail.com>
2024-09-19 15:15:38 +0600

    add money to balance page added

--- js/home.js
+++ js/home.js
@@ -3,12 +3,34 @@
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     //   prevent page reload after submit
     event.preventDefault();
-    // get money to be added to the account
+    //step-2 get money to be added to the account
     const addMoneyInput = document.getElementById('input-add-money').value;
     console.log(addMoneyInput);
 
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    // step-3 verify the pin number(wrong way not right way to validate)
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+        //step-4 get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5 add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // update the balance in the ui/dom
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('failed to add money ! please try again')
+
+}
    
 });
