1581885ef7fea5f40ebd614b326cc4d7de51a1a6
shaoon09 <iiucclass1@gmail.com>
2024-09-19 11:46:35 +0600

    home page added

--- js/login.js
+++ js/login.js
@@ -6,14 +6,26 @@
 // })
 //step -1 set event handler
 document.getElementById('button-login').addEventListener('click', function(event){
-    // prevent default behavior ( prevent reloading browser)
+    //step-2 prevent default behavior ( prevent reloading browser)
     event.preventDefault();
-    console.log('login button clicked');
-
-    // step - 3 get the phone number
+    
+    // step - 3 get the phone number & pin number
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber);
+    // step-4 validate phone & pin
+    // this is temporary
+    if(phoneNumber=== '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+        
+        // allow to user to use the website
+    }
+    else{
+        alert('wrong phone number & pin number')
+    }
 
 
     
